707007f2a2f0eac0198281065678df1a75a6de0b
Blazious <blaziousmwambuwa@gmail.com>
2025-09-08 14:50:19 +0300

    added templates and urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Book, Library
 
-# Function-based view
+# Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-based view
+# Class-based view: library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+# Signup view (user registration)
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log the user in after signup
+            return redirect("list_books")  # redirect to books list
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/signup.html", {"form": form})
+
