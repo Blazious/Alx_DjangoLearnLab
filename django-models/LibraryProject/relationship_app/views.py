dd2316a6eb011e96fb538ac2050412e4a44b1c60
Blazious <blaziousmwambuwa@gmail.com>
2025-09-08 15:39:05 +0300

    added RBAC

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 from .models import Book, Library
+
 
 # Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
+
 # Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
 
 # Must be named 'register' for checker
 def register(request):
@@ -26,5 +30,22 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+
+
+# Role-based views
+@user_passes_test(lambda u: hasattr(u, "userprofile") and u.userprofile.role == "Admin")
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(lambda u: hasattr(u, "userprofile") and u.userprofile.role == "Librarian")
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(lambda u: hasattr(u, "userprofile") and u.userprofile.role == "Member")
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
+
 
 
