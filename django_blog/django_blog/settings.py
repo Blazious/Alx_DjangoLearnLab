6881fe3f4d4291f7b8ec8e2d3a66214dff42275c
Blazious Mwambuwa <blaziousmwmabuwa@gmail.com>
2025-09-29 07:52:11 +0300

    setup db

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,13 +73,26 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import os
 
+if os.getenv("USE_SQLITE", "1") == "1":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'fake_blog_db',
+            'USER': 'fake_user',
+            'PASSWORD': 'fake_password',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
