cb01ad0e345a1404cc5ee1a5dbe3311ba51400ee
Blazious Mwambuwa <blaziousmwmabuwa@gmail.com>
2025-09-29 09:06:19 +0300

    added taggit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from taggit.managers import TaggableManager
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -14,8 +15,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
-
+    tags = TaggableManager()
     def __str__(self):
         return self.title
     
